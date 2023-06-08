57502a7cf1a71e6918c258e9ad411873b45ce88c
motiz007 <francismototo@gmail.com>
2023-06-08 13:45:25 -0700

    python func that adds

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
+import add from add_0
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
