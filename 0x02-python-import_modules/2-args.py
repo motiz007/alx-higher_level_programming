a562c9aa48dee2c8293f02bef8468774780e5b92
motiz007 <francismototo@gmail.com>
2023-06-08 14:26:46 -0700

    addition of argv printer

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    ac = len(argv) - 1
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("{:d} argument:".format(ac))
+        print("{:d}: {}".format(ac, argv[1]))
+    else:
+        print("{:d} arguments:".format(ac))
+        num = 1 
+        while num < ac + 1:
+            print("{:d}: {}".format(num, argv[num]))
+            num += 1
