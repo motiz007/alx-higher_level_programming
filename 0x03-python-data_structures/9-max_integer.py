b16c74d697f14d167137f849d3aa6b132a270055
motiz007 <francismototo@gmail.com>
2023-06-11 09:47:19 -0700

    find max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    else:
-        maxV = my_list[0]
-        for item in my_list:
-            if item > maxV:
-                maxV = item
-        return maxV
+    maxV = my_list[0]
+    for item in my_list:
+        if item > maxV:
+            maxV = item
+    return maxV
