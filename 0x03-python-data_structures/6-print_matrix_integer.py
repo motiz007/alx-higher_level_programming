4b2b56b1e698f310d70731d864002e12663b07b5
motiz007 <francismototo@gmail.com>
2023-06-11 08:18:25 -0700

    print 2d matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
+    if matrix is None:
         print()
     else:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end="")
+        for row in matrix:
+            for item in row:
+                print("{:d}".format(item), end="")
             print()
