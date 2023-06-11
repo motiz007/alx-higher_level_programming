41eb94640a64415edce796fd4b2beffaecc446c4
motiz007 <francismototo@gmail.com>
2023-06-11 09:22:47 -0700

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,13 +2,10 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    len_a = len(list_a)
-    len_b = len(list_b)
-
-    if len_a < 2:
-        list_a.append(0)
-    if len_b < 2:
-        list_b.append(0)
-
-    list_total = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
-    return tuple(list_total)
+    if len(list_a) < 2:
+        list_a.extend([0] * (2 - len(list_a)))
+    if len(list_b) < 2:
+        list_b.extend([0] * (2 - len(list_b)))
+    sum_1 = list_a[0] + list_b[0]
+    sum_2 = list_a[1] + list_b[1]
+    return sum_1, sum_2
