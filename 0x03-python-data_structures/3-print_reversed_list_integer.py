e5233d283034dda003f18a1611213771c06f9ca3
motiz007 <francismototo@gmail.com>
2023-06-11 05:23:13 -0700

    print list in reverse order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    l_idx = len(my_list) - 1
+    while l_idx >= 0:
+        print("{:d}".format(my_list[l_idx]))
+        l_idx -= 1
