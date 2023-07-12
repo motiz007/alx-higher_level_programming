9dd87a96cb76f6c94e30dbaf7ad1f84abc27ebb4
motiz007 <francismototo@gmail.com>
2023-07-12 23:38:14 +0300

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,14 @@
 
 
 import sys
-from os.path import exists
-
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 filename = "add_item.json"
-if exists(filename):
+try:
     old_args = load_from_json_file(filename)
-    old_args.extend(sys.argv[1:])
-else:
+except FileNotFoundError:
     old_args = []
+old_args.extend(sys.argv[1:])
 save_to_json_file(old_args, filename)
