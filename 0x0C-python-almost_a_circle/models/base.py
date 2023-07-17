7ff47265931975878c403836f9d209d9a3a56cca
motiz007 <francismototo@gmail.com>
2023-07-17 16:17:09 -0700

    corecction

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,13 +29,45 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        f_name = cls.__name__ + ".json"
-        with open(f_name, mode="w", encoding="utf-8") as fd:
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
             if list_objs is None:
-                fd.write("[]")
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
-            dict_list = []
-            for obj in list_objs:
-                dict_list.append(obj.to_dictionary())
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
 
-            fd.write(Base.to_json_string(dict_list))
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary of attributes.
+
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
